6b773cfe901d76f7f21d66cffddd16ef0abd46bb
ViktoriyaO <43511927+ViktoriyaO@users.noreply.github.com>
2020-05-03 15:02:53 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,24 +1,24 @@
 CREATE TABLE Diamond(
     diamond_index INT NOT NULL,
-    color CHAR(1) NOT NULL,
-    cut VARCHAR2(9) NOT NULL,
-    clarity VARCHAR2(4) NOT NULL,
-    carat_weight VARCHAR2(4) NOT NULL);
+    color VARCHAR2(250) NOT NULL,
+    cut VARCHAR2(250) NOT NULL,
+    clarity VARCHAR2(250) NOT NULL,
+    carat_weight VARCHAR2(6) NOT NULL);
 ALTER TABLE Diamond
     ADD CONSTRAINT diamond_index_pk PRIMARY KEY(diamond_index);
 
 CREATE TABLE Description(
-    color CHAR(1) NOT NULL);
+    color VARCHAR2(250) NOT NULL);
 ALTER TABLE Description
     ADD CONSTRAINT color_pk PRIMARY KEY(color);    
     
 CREATE TABLE Category(
-    cut VARCHAR2(9) NOT NULL);
+    cut VARCHAR2(250) NOT NULL);
 ALTER TABLE Category
     ADD CONSTRAINT cut_pk PRIMARY KEY(cut);    
     
 CREATE TABLE Clarity(
-    clarity VARCHAR2(4) NOT NULL);
+    clarity VARCHAR2(250) NOT NULL);
 ALTER TABLE Clarity
     ADD CONSTRAINT clarity_pk PRIMARY KEY(clarity);
     
