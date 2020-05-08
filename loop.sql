22776cd01a20287f575a8978c06a72ac7882c76f
ViktoriyaO <43511927+ViktoriyaO@users.noreply.github.com>
2020-05-08 13:28:48 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,15 +1,15 @@
-DECLARE 
+9DECLARE 
     items_count INT NOT NULL DEFAULT 1; 
 BEGIN
     FOR i IN 1..items_count LOOP
         INSERT INTO Diamond (diamond_index, color, cut, clarity, carat_weight)
-            VALUES ('53941', 'F', 'Ideal', 'IF', '0.2'); 
+            VALUES ('9', 'F', 'Ideal', 'IF', '0.2'); 
 
         INSERT INTO Diamond (diamond_index, color, cut, clarity, carat_weight)
-            VALUES ('53942', 'J', 'Premium', 'VS2', '0.31');
+            VALUES ('10', 'J', 'Premium', 'VS2', '0.31');
 
         INSERT INTO Diamond (diamond_index, color, cut, clarity, carat_weight)
-            VALUES ('53943', 'I', 'Good', 'VS1', '0.52'); 
+            VALUES ('11', 'I', 'Good', 'VS1', '0.52'); 
     END LOOP;
 END;
     
